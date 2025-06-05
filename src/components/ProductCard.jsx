bbc4cb69c7d5dff605b46f8a65dea87de76dd1c0
Katya Weicht <katya.weicht@gmail.com>
2025-06-05 12:35:37 -0600

    Apply conditional styling to out of stock products

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -2,13 +2,34 @@ import React from 'react'
 import styles from '../styles/ProductCard.module.css'
 
 const ProductCard = ({ product, addItemsToCart }) => {
+  let card;
+  if (product.inStock) {
+    card = styles.inStock;
+  } else {
+    card = styles.outOfStock;
+  }
+
+  let status;
+  if (product.inStock) {
+    status = styles.inStockStatus
+  } else {
+    status = styles.outOfStockStatus
+  }
+
+  let info;
+  if (product.inStock) {
+    info = styles.inStockInfo
+  } else {
+    info = styles.outOfStockInfo
+  }
+
   return (
     <div
-      className={`${styles.card} ${!product.inStock ? styles.outOfStock : ''}`}
+      className={card}
     >
-      <h3>{product.name}</h3>
-      <p>Price: {product.price}</p>
-      <p>Status: {product.inStock ? 'In Stock' : 'Out of Stock'}</p>
+      <h3 className = {info}>{product.name}</h3>
+      <p className = {info}>Price: {product.price}</p>
+      <p className={status}> Status: {product.inStock ? 'In Stock' : 'Out of Stock'}</p>
 
       <button onClick={()=> addItemsToCart(product)} disabled={!product.inStock}>Add to Cart</button>
     </div>
